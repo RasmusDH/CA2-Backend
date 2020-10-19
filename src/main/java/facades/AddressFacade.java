0d5e9e0e07b3d65cade55c84fa6b6debc7eb3c31
malthew <malthe.woschek@gmail.com>
2020-10-19 20:18:26 +0200

    Created facade class for address + cityinfo with methods.

--- src/main/java/facades/AddressFacade.java
+++ src/main/java/facades/AddressFacade.java
@@ -1,12 +1,19 @@
 package facades;
 
 import DTO.AddressDTO;
+import DTO.CityInfoDTO;
+import DTO.PersonDTO;
 import entities.Address;
+import entities.Person;
 import exceptions.AlreadyExistsException;
+import exceptions.NotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 /**
  *
@@ -38,16 +45,15 @@ public class AddressFacade {
         return emf.createEntityManager();
     }
     
-    //TODO Remove/Change this before use
-    public long getRenameMeCount(){
+    
+    public List<CityInfoDTO> getAllZipCodes() {
         EntityManager em = emf.createEntityManager();
-        try{
-            long renameMeCount = (long)em.createQuery("SELECT COUNT(r) FROM RenameMe r").getSingleResult();
-            return renameMeCount;
-        }finally{  
+        try {
+            List<CityInfoDTO> list = em.createQuery("SELECT c FROM CityInfo c").getResultList();
+            return list;
+        } finally {
             em.close();
         }
-        
     }
     
     public AddressDTO createAddress(AddressDTO address) throws AlreadyExistsException {
@@ -62,6 +68,45 @@ public class AddressFacade {
             return address;
         } catch (Exception ex) {
             throw new AlreadyExistsException("This address exists in the database.");
+        } finally {
+            em.close();
+        }
+    }
+    
+    public AddressDTO findAddress(String street) throws NotFoundException {
+        EntityManager em = emf.createEntityManager();
+        Address entaddress = null;
+        try {
+            em.getTransaction().begin();
+            TypedQuery<Address> query = em.createQuery("SELECT a FROM Address a WHERE"
+                    + " a.street = :address", Address.class)
+                    .setParameter("address", street);
+            em.getTransaction().commit();
+            entaddress = query.getSingleResult();
+            return new AddressDTO(entaddress);
+        } catch (Exception e) {
+            throw new NotFoundException("No address found");
+        } finally {
+            em.close();
+        }
+    }
+    
+    public List<PersonDTO> getAllPersonsWithZip(int zipCode) throws NotFoundException {
+        EntityManager em = emf.createEntityManager();
+        try {
+            List<Person> persons = em.createQuery("SELECT p FROM Person p "
+                    + "JOIN p.address pa JOIN pa.cityInfo pac WHERE pac.zipCode = :zipCode", Person.class)
+                    .setParameter("zipCode", zipCode)
+                    .getResultList();
+
+            List<PersonDTO> persondtos = new ArrayList();
+            for (Person person : persons) {
+                persondtos.add(new PersonDTO(person));
+            }
+
+            return persondtos;
+        } catch (NoResultException ex) {
+            throw new NotFoundException("Persons with given Zip Code could not be found");
         } finally {
             em.close();
         }
